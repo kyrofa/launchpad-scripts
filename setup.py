440950d84d321eda81689ee3338223e753aacb3a
Kyle Fazzari <kyrofa@ubuntu.com>
2021-02-20 07:07:06 -0800

    Bump version to 0.3.1
    Add launchpadlib to install_requires.
    
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="cla-check",
-    version="0.3",
+    version="0.3.1",
+    install_requires=["launchpadlib"],
     packages=find_packages(),
     entry_points={
         "console_scripts": [
